261eac2d36e8eaf2a93f5543440dbf679e723b18
Michał Czyż <mike@c2yz.com>
2024-12-27 17:04:25 +0100

    feat: nixified

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages.${system}.dwm = pkgs.stdenv.mkDerivation rec {
+      pname = "dwm";
+      version = "custom";
+      src = ./.;
+      
+      buildInputs = with pkgs; [
+        xorg.libX11
+        xorg.libXft
+        xorg.libXinerama
+      ];
+
+      makeFlags = [ "CC=${pkgs.gcc}/bin/gcc" ];
+
+      installPhase = ''
+        mkdir -p $out/bin
+        cp dwm $out/bin/
+      '';
+
+      meta = with pkgs.lib; {
+        description = "Dynamic Window Manager";
+        license = licenses.mit;
+        maintainers = [ ];
+      };
+    };
+
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        gnumake
+        gcc
+        xorg.libX11
+        xorg.libX11.dev
+        xorg.libXft
+        xorg.libXinerama
+      ];
+      
+      shellHook = ''
+        echo "Development shell for dwm ready."
+      '';
+    };
+  };
+}
